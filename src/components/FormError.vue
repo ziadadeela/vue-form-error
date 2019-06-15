207f189f792263d0f5ad3d429ef1f5625b8701b6
Ziad Adeela <zeiad.suliman@gmail.com>
2019-06-15 21:47:13 +0300

    fix Empty array error case.

--- src/components/FormError.vue
+++ src/components/FormError.vue
@@ -1,38 +1,39 @@
 <template>
-  <div v-if="errors" class="has-error">
-    <slot></slot>
-    <p v-for="error in errors" class="error-message" :key="error">
-      {{error}}
-    </p>
-  </div>
-  <div v-else>
-    <slot></slot>
-  </div>
+    <div v-if="errors && errors.length >0" class="has-error">
+        <slot></slot>
+        <p v-for="error in errors" class="error-message" :key="error">
+            {{error}}
+        </p>
+    </div>
+    <div v-else>
+        <slot></slot>
+    </div>
 
 </template>
 
 <script>
-  export default {
+    export default {
         props: {
-          errors:{
-            type: Array,
-            default: ()=>[]
-          }
+            errors: {
+                type: Array,
+                default: () => []
+            }
         },
     }
 </script>
 
 <style>
-  .has-error {
-    color: #ff4941;
-  }
-  /*.has-error input,.has-error select{*/
-  /*  border-color: red;*/
-  /*}*/
-  .has-error .error-message{
-    /*text-align: center;*/
-    color: #ff4941;
-    font-size: 13px;
-    margin: 1px;
-  }
+    .has-error {
+        color: #ff4941;
+    }
+
+    /*.has-error input,.has-error select{*/
+    /*  border-color: red;*/
+    /*}*/
+    .has-error .error-message {
+        /*text-align: center;*/
+        color: #ff4941;
+        font-size: 13px;
+        margin: 1px;
+    }
 </style>
